87e7ea119fd2794262cb11d313a5372af8bb6002
ErickLauretti <erick.nighyt@hotmail.com>
2024-07-10 23:13:02 -0300

    desafio 1 feito

--- desafio1.py
+++ desafio1.py
@@ -1,1 +1,10 @@
-"Opa!"
+def calculo_media():
+    nome = input("Escreva seu nome: ")
+    nota1 = int(input("Digite a primeira nota: "))
+    nota2 = int(input("Digite a segunda nota: "))
+    nota3 = int(input("Digite a terceira nota: "))
+    nota4 = int(input("Digite a quarta nota: "))
+    media = (nota1 + nota2 + nota3 + nota4) / 4
+    return f"Aluno = {nome} // Media = {media}"
+
+print(calculo_media())
